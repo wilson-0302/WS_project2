6e15e445cc7fcfd73ccb00bcdef7d817617ab18e
wilson-0302 <gksmf030302@handong.ac.kr>
2025-11-24 23:13:14 +0900

    hotfix

--- src/main/java/com/exmaple/common/JDBCUtil.java
+++ src/main/java/com/exmaple/common/JDBCUtil.java
@@ -4,20 +4,19 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class JDBCUtil {
-    private static Connection con=null;
-    public static Connection getConnection(){
 
-        try{
-            if(con == null) {
-                Class.forName("org.mariadb.jdbc.Driver");
-                con= DriverManager.getConnection("jdbc:mariadb://walab.handong.edu:3306/W25_22200320"
-                        ,"W25_22200320"
-                        ,"Dumie3");
-            }
-            // System.out.println("db 연결 성공");
-        }catch(Exception e){
-            System.out.println(e);
-            // System.out.println("db 연결 실패");
+    public static Connection getConnection() {
+        Connection con = null;
+
+        try {
+            Class.forName("org.mariadb.jdbc.Driver");
+            con = DriverManager.getConnection(
+                    "jdbc:mariadb://walab.handong.edu:3306/W25_22200320",
+                    "W25_22200320",
+                    "Dumie3"
+            );
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
         return con;
